4043b3834dafe8647c6a9ee723763b4116d59367
DevanshGupta15 <devanshguptahero@gmail.com>
2024-12-23 17:34:21 +0530

    prod ready

---
+++ frontend/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{d as a}from"./chunk-4CME4B2D.mjs";import"./chunk-YLCRSHE4.mjs";import"./chunk-4KZRSLMR.mjs";export{a as default};
--- index.js
+++ index.js
@@ -20,6 +20,13 @@ app.use(cors());
 app.use(express.json());
 app.use('/api', apiRoutes);
 
+
+app.use(express.static('frontend/browser'))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend/browser', 'index.html'));
+  });
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
